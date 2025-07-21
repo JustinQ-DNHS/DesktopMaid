fe9f75399b82be23b34091b0f28c75e38a2998da
JustinQ-DNHS <justin.q224@icloud.com>
2025-07-21 02:50:36 -0700

    logic rewrite, pushing for different gif interpretation

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,20 +2,36 @@ using System;
 using ReactiveUI;
 
 namespace DesktopMaid.ViewModels;
-
+// I'll be honest, not even I know how the setup works
 public partial class MainWindowViewModel : ViewModelBase
 {
-    private string[] filePaths = { "idle.gif", "low.gif", "lowlow.gif", };
+    // Array of all filepaths, will matter in the future
+    private string[] filePaths =
+    {
+        "chairpop.gif", "chatbox.png", "class.gif",
+        "heart.png", "idle.gif", "low.gif", "lowlow.gif",
+        "lowlowlow.gif", "scrub.gif", "walking_neg.gif", "walking_pos.gif", "watch.gif"
+    };
+    // Private string ref, stores actual data
     private string _gifPath = "avares://DesktopMaid/Assets/idle.gif";
+    // Public string ref, data comes from here
     public string GifPath
     {
         get => _gifPath;
         set => this.RaiseAndSetIfChanged(ref _gifPath, $"avares://DesktopMaid/Assets/{value}");
     }
-    public void ChangeGif()
+    // Sets animations when called, re-write later
+    public void ChangeGif(string animation)
     {
-        GifPath = GifPath == "avares://DesktopMaid/Assets/idle.gif" 
-            ? "lowlow.gif" 
-            : "idle.gif";
+        GifPath = animation switch
+        {
+            "idle" => filePaths[4],
+            "pickup0" => filePaths[5],
+            "pickup1" => filePaths[6],
+            "pickup2" => filePaths[7],
+            "walkNeg" => filePaths[9],
+            "walkPos" => filePaths[10],
+            _ => GifPath
+        };
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Media;
+using Avalonia.Threading;
 
 using DesktopMaid.ViewModels;
 using Tmds.DBus.Protocol;
@@ -11,6 +12,9 @@ namespace DesktopMaid.Views;
 
 public partial class MainWindow : Window, INotifyPropertyChanged
 {
+    // Create timer for periodic function, using System.ComponentModel
+    private readonly DispatcherTimer _timer;
+    // Main Function
     public MainWindow()
     {
         InitializeComponent();
@@ -18,15 +22,28 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         TransparencyLevelHint = new[] { WindowTransparencyLevel.Transparent };
         Background = Brushes.Transparent;
         DataContext = new MainWindowViewModel();
+        // Gives a value to _timer, making it a new DispathcerTimer object
+        _timer = new DispatcherTimer
+        {
+            // Parameters for DispatherTimer interval set to 200 milliseconds
+            Interval = TimeSpan.FromMilliseconds(200)
+        };
+        // Starts timer and whenever smth occurs run Periodic function
+        _timer.Tick += (_, _) => Periodic();
+        // Start the timer
+        _timer.Start();
     }
+    // Run function when maid is clicked in any way, interprets it and runs appropriate function
     private void Window_PointerPress(object? sender, PointerPressedEventArgs e)
     {
         if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
         {
+            // Checks if DataContent exists and if it does use vm as a placeholder for easy reference
             if (DataContext is MainWindowViewModel vm)
             {
+                // Drags stuff and changes GIF source
                 BeginMoveDrag(e);
-                vm.ChangeGif();
+                vm.ChangeGif("pickup1");
             }
         }
         if (e.GetCurrentPoint(this).Properties.IsRightButtonPressed)
@@ -34,26 +51,30 @@ public partial class MainWindow : Window, INotifyPropertyChanged
             this.Close();
         }
     }
+    // Runs when the leftclick is released
     private void Window_PointerRelease(object? sender, PointerReleasedEventArgs e)
     {
         if (e.GetCurrentPoint(this).Properties.PointerUpdateKind == PointerUpdateKind.LeftButtonReleased)
         {
+            // ~~~
             if (DataContext is MainWindowViewModel vm)
             {
-                vm.ChangeGif();
+                vm.ChangeGif("idle");
             }
         }
     }
+    // Creates a random var, using System;
     Random rand = new Random();
+    // Periodic function run everytime timer goes through
     private void Periodic()
     {
         if (DataContext is MainWindowViewModel vm)
         {
-            // Determine random action
+            // Determine random action and runs it
             int Temp = rand.Next(0, 5);
-            if (Temp < 2) { vm.ChangeGif(); }
-            else if (Temp < 3) { vm.ChangeGif(); }
-            else { vm.ChangeGif(); }
+            if (Temp < 2) { vm.ChangeGif("idle"); }
+            else if (Temp < 3) { vm.ChangeGif("walkNeg"); Position = new Avalonia.PixelPoint(Position.X + 1, Position.Y); }
+            else { vm.ChangeGif("walkPos"); Position = new Avalonia.PixelPoint(Position.X - 1, Position.Y); }
         }
     }
 }
